93d4be228d04c670f1cfa3bdc9386a7a42a40c2a
Bruno Gago <92176548+BrunoGago@users.noreply.github.com>
2023-11-10 21:28:42 -0300

    Feature: criação do projeto base

---
+++ src/main/java/com/fiap/Plant4U/Plant4UApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.Plant4U;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Plant4UApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Plant4UApplication.class, args);
+	}
+
+}
